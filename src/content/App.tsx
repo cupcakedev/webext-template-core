3f5ce6d7069f0ffa43172eb2b9c5cd1be8a1f7fc
Maxim Zakharov <maksim.zakharov@cupcakedev.com>
2022-05-18 11:57:29 +0700

    Ignore build error

--- src/content/App.tsx
+++ src/content/App.tsx
@@ -28,6 +28,7 @@ const App = () => {
 
     if (!Route) return null;
 
+    // @ts-ignore
     // eslint-disable-next-line react/jsx-props-no-spreading
     return <Route.Component {...Route.props} />;
 };
