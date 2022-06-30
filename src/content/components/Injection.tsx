c6f07ad32e3451f7aac44a2e148a180471ae9d28
Maxim Zakharov <maksim.zakharov@cupcakedev.com>
2022-06-30 18:22:26 +0700

    Fix react import

--- src/content/components/Injection.tsx
+++ src/content/components/Injection.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import { ReactNode, useLayoutEffect, useState } from 'react';
+import React, { ReactNode, useLayoutEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 export interface InjectionProps {
