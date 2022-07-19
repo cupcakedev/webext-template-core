0dad31d2403ed188f65ea7ff8a2d3b611bc21479
Maxim Zakharov <maksim.zakharov@cupcakedev.com>
2022-07-19 20:51:25 +0700

    Rename storage hook file, remove example

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,4 @@ export {
     getTabCaller,
 } from './bridge/tabsEvents';
 export { RouteInterface, Router } from './pages/content/router';
-export { createHookUseChromeStorage } from './hooks/useChromeStorage';
+export { createHookUseChromeStorage } from './hooks/createHookUseChromeStorage';
